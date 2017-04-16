658d24b16e41a25b8df17cacb09903c6fd190cdc
sumonkarmakar <suman.talkto@gmail.com>
2017-04-16 10:12:45 +0530

    Revert "Revert "Revert "Project Final"""
    This reverts commit bb6057e348927b634a3be1054eb5bb80095ccf62.

--- project code/crud_practice_12_02_17/public/js/app.js
+++
@@ -1,3 +0,0 @@
-angular.module('sampleApp', ['ngRoute','appRoutes','MainCtrl','MovieCtrl',
-	'CityCtrl','TheatreCtrl','ShowMappingCtrl','MovieDetailCtrl',
-	    'BookingCtrl','ConfirmBookingCtrl']);
